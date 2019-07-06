f3f2dcf5d9beb47dfd3416fb7e49d74ba2145d6a
OlegBezr <obezrukavnikov@gmail.com>
2019-07-07 02:07:22 +0300

    ML stuff

--- routes.py
+++ routes.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import joblib
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__) #create instance of the app
 
 @app.route("/") #path to our app after IP->Port
 def index(): #function for app to call
-	return render_template("index.html")
+	#I save data for prediction in csv file
+	info = pd.read_csv("info.csv")
+	predict_param = info.values.tolist()[0][1:]
 
-if __name__ == "__main__":
-	app.run(debug = True)
+	prediction = np.squeeze(model.predict([predict_param]).round(1))
+	return render_template("index.html", prediction = prediction)
+
+if __name__ == '__main__':
+    model = joblib.load('regr.pkl')
+    app.run(debug=True)
