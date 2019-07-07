2fee22160ab5b42d441cddc0b7e27e1a406e07d7
OlegBezr <obezrukavnikov@gmail.com>
2019-07-07 03:20:27 +0300

    I begg

--- routes.py
+++ routes.py
@@ -5,15 +5,14 @@ import numpy as np
 
 app = Flask(__name__) #create instance of the app
 
-model = joblib.load('regr.pkl')
-info = pd.read_csv("info.csv")
-
 @app.route("/") #path to our app after IP->Port
 def index(): #function for app to call
-	predict_param = info.values.tolist()[0][1:]
-	prediction = np.squeeze(model.predict([predict_param]).round(1))
 	return render_template("index.html", prediction = prediction)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     #I save data for prediction in csv file
-    app.run(debug=True)
+	model = joblib.load("regr.pkl")
+	info = pd.read_csv("info.csv")
+	predict_param = info.values.tolist()[0][1:]
+	prediction = np.squeeze(model.predict([predict_param]).round(1))
+	app.run(debug=True)
