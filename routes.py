2281172a4923ff7a4c7339c900b951e961871ceb
OlegBezr <obezrukavnikov@gmail.com>
2019-07-07 08:36:56 +0300

    Try

--- routes.py
+++ routes.py
@@ -3,19 +3,18 @@ import joblib
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_boston
-from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeRegressor
-
-model = joblib.load("regr.pkl")
-info = pd.read_csv("info.csv")
 
 app = Flask(__name__) #create instance of the app
 
 @app.route("/") #path to our app after IP->Port
 def index(): #function for app to call
+	model = joblib.load("regr.pkl")
+	info = pd.read_csv("info.csv")
+
 	predict_param = info.values.tolist()[0][1:]
+
+	print(predict_param)
+
 	prediction = np.squeeze(model.predict([predict_param]).round(1))
 	return render_template("index.html", prediction = prediction)
 
