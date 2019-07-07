071044b08d6db294bd850dd302c8f9b124655a5a
OlegBezr <obezrukavnikov@gmail.com>
2019-07-07 19:14:14 +0300

    Script structure work

--- routes.py
+++ routes.py
@@ -8,12 +8,11 @@ app = Flask(__name__) #create instance of the app
 
 @app.route("/") #path to our app after IP->Port
 def index(): #function for app to call
-	model = joblib.load("regr.pkl")
-	info = pd.read_csv("info.csv")
 	predict_param = info.values.tolist()[0][1:]
 	prediction = np.squeeze(model.predict([predict_param]).round(1))
 	return render_template("index.html", prediction = prediction)
 
 if __name__ == "__main__":
-    #I save data for prediction in csv file
+	model = joblib.load("regr.pkl")
+	info = pd.read_csv("info.csv") #I save data for prediction in csv file
 	app.run(debug=True)
