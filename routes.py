df82ca47cd2995e134f6d44e4fc3a0c0fecd2f3f
OlegBezr <obezrukavnikov@gmail.com>
2019-07-07 19:28:28 +0300

    Proper heroku work

--- routes.py
+++ routes.py
@@ -8,11 +8,11 @@ app = Flask(__name__) #create instance of the app
 
 @app.route("/") #path to our app after IP->Port
 def index(): #function for app to call
+	model = joblib.load("regr.pkl")
+	info = pd.read_csv("info.csv") #I save data for prediction in csv file
 	predict_param = info.values.tolist()[0][1:]
 	prediction = np.squeeze(model.predict([predict_param]).round(1))
 	return render_template("index.html", prediction = prediction)
 
 if __name__ == "__main__":
-	model = joblib.load("regr.pkl")
-	info = pd.read_csv("info.csv") #I save data for prediction in csv file
 	app.run(debug=True)
