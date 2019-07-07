c384a6de0af85f4d7c2d621fbe483cb9beb694bf
OlegBezr <obezrukavnikov@gmail.com>
2019-07-07 03:13:33 +0300

    HATRED

--- routes.py
+++ routes.py
@@ -5,8 +5,8 @@ import numpy as np
 
 app = Flask(__name__) #create instance of the app
 
-info = ''
-model = ''
+model = joblib.load('regr.pkl')
+info = pd.read_csv("info.csv")
 
 @app.route("/") #path to our app after IP->Port
 def index(): #function for app to call
@@ -15,7 +15,5 @@ def index(): #function for app to call
 	return render_template("index.html", prediction = prediction)
 
 if __name__ == '__main__':
-    model = joblib.load('regr.pkl')
     #I save data for prediction in csv file
-    info = pd.read_csv("info.csv")
     app.run(debug=True)
